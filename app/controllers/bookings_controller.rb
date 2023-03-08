502b20c81488132497f55cc4388201239e7f4ecb
Ahmad Shakib Omar <shakib.omar@hotmail.com>
2023-03-08 16:16:42 +0100

    Booking controller has completed

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,48 @@
+class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def edit
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking, notice: "Booking was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @booking.update(booking_params)
+      redirect_to @booking, notice: "Booking was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to bookings_url, notice: "Booking was successfully destroyed."
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:name)
+  end
+end
