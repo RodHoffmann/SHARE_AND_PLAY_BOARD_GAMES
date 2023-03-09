0e262435a4079f94483627f266d501abcc3a3dfb
Rodrigo Hoffmann <rhoffmannp@gmail.com>
2023-03-09 10:56:03 +0100

    Nested the booking routes inside games and created the users controller with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :games
-  resources :bookings
+  resources :games do
+    resources :bookings, only: %i[new create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
