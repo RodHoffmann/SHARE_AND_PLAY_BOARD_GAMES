545872ec9f79b91a4115b8ab79bca6201e813a30
Rodrigo Hoffmann <rhoffmannp@gmail.com>
2023-03-09 11:06:54 +0100

    Migration added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_150344) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_095658) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_150344) do
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -46,6 +53,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_150344) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -53,4 +62,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_150344) do
   add_foreign_key "bookings", "games"
   add_foreign_key "bookings", "users"
   add_foreign_key "games", "users"
+  add_foreign_key "profiles", "users"
 end
