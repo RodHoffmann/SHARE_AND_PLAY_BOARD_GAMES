f40875efc5cbc00547ac4d3d0cfec14d947b4b3b
zain srio <zein.srio@gmail.com>
2023-03-09 12:07:31 +0100

    added the game view/index&show&new

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -17,9 +17,10 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.new(game_params)
-
+    @game.user = current_user
     if @game.save
-      redirect_to profile_path, notice: "Game was successfully created."
+      redirect_to games_path, notice: "Game was successfully created."
+
     else
       render :new, status: :unprocessable_entity
     end
