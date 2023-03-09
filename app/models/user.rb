f02af282b892a0274fba8577c2a6478cb514343f
Rodrigo Hoffmann <rhoffmannp@gmail.com>
2023-03-09 11:01:23 +0100

    Created the Profile Model, and Profiles Controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :games, dependent: :destroy
   has_many :bookings, dependent: :destroy
+  has_one :profile, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
