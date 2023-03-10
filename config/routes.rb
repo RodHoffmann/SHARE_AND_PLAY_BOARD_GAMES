9999d476937b0c1c8fc0c16386d2e560ff139daa
zain srio <zein.srio@gmail.com>
2023-03-10 14:34:24 +0100

    added a footer,background image and updateded the routes for the profile page and added a view for it

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users do
-    resources :profiles, only: %i[show]
-  end
+  devise_for :users
   root to: "pages#home"
   resources :games do
     resources :bookings, only: %i[new create]
   end
+  get "profile",to: "profiles#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
