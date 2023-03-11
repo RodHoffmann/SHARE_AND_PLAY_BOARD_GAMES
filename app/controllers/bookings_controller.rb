276b2b79d82422b52eac0ebf24d6dfa00a1ffb4e
Rodrigo Hoffmann <rhoffmannp@gmail.com>
2023-03-11 12:48:42 +0100

    Change the game show view for the new booking view using the same code and centered the form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,16 +10,20 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
+    @game =  Game.find(params[:game_id])
   end
 
   def edit
   end
 
   def create
+    raise
     @booking = Booking.new(booking_params)
-
+    @game =  Game.find(params[:game_id])
+    @booking.game = @game
+    @booking.user = current_user
     if @booking.save
-      redirect_to profile_path, notice: "Booking was successfully created."
+      redirect_to games_path, notice: "Booking was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
