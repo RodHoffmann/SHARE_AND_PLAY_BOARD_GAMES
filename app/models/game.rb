4f4d6964b2cc665763b9baa5f2ebb94aa5a7d0c3
Ahmad Shakib Omar <shakib.omar@hotmail.com>
2023-03-15 16:23:09 +0100

    Seed Images to game has completed

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,6 @@ class Game < ApplicationRecord
   validates :category, presence: true
   validates :number_players, presence: true
   validates :day_price, presence: true
-  has_one_attached :photo
+  has_one_attached :image
   belongs_to :user
 end
